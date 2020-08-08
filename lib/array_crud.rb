6b86399036d37502a2a81c5134a02d947bec2bf5
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-08 23:54:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  dog_breeds = ["Husky", "Golden Retriever", "German Shepard", "Beagle"]
 end
 
 def add_element_to_end_of_array(array, element)
