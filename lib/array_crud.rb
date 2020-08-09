c643b2bfd7ef9ed80ff7b58106d1ad0cdd36c562
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-09 00:01:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,6 @@ end
 def add_element_to_end_of_array
   dog_breeds = ["Husky", "Golden Retriever", "German Shepard", "Beagle"]
   dog_breeds << "Malamute"
-  p dog_breeds
 end
 
 def add_element_to_start_of_array(array, element)
